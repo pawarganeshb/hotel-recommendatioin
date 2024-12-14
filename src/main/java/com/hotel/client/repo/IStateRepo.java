5446b218995871fda00b333b50734698b6810b91
pawarganeshb <pawarganeshb@123.com>
2024-12-14 11:18:33 +0530

    added

--- src/main/java/com/hotel/client/repo/IStateRepo.java
+++ src/main/java/com/hotel/client/repo/IStateRepo.java
@@ -1,7 +1,4 @@
 package com.hotel.client.repo;
 
-import com.hotel.client.entity.StateEntity;
-
 public interface IStateRepo {
-public boolean addState(StateEntity stateEntity);
 }
--- src/main/java/com/hotel/client/repo/StateRepoImp.java
+++ src/main/java/com/hotel/client/repo/StateRepoImp.java
@@ -1,12 +1,7 @@
 package com.hotel.client.repo;
 
-import com.hotel.client.entity.StateEntity;
-
 public class StateRepoImp implements IStateRepo {
 
-	@Override
-	public boolean addState(StateEntity stateEntity) {
-		return false;
-	}
+	
 
 }
