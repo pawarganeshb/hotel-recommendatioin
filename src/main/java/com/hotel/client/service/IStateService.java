1de8ca3a0c3bd5718b5afc8639eed024712ae3b8
pawarganeshb <pawarganeshb@123.com>
2024-12-15 11:58:20 +0530

    added

--- src/main/java/com/hotel/client/service/IStateService.java
+++ src/main/java/com/hotel/client/service/IStateService.java
@@ -1,10 +1,10 @@
-package com.hotel.client.repo;
+package com.hotel.client.service;
 
 import java.util.List;
 
 import com.hotel.client.entity.StateEntity;
 
-public interface ISateRepo {
+public interface IStateService {
 	public boolean addState(StateEntity stateEntity);
 
 	public boolean deleteState(int stateId);
