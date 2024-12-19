ccc2fc9cef841aaf310b89ab9a3c73ba355b57d1
sonawaneshivam <shiv.sonawane09@gmail.com>
2024-12-19 18:11:16 +0530

    kuch tho hey

--- src/main/java/com/hotel/client/AdminOperation/DistrictEntity.java
+++ src/main/java/com/hotel/client/AdminOperation/DistrictEntity.java
@@ -1,12 +1,14 @@
 package com.hotel.client.AdminOperation;
 
+import com.hotel.client.entity.StateEntity;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class DistrictEntity {
+public class DistrictEntity extends StateEntity{
 	
 	
 	private int distId;
